8faa558fcff5a116d73f01b1c4690fa01d17ed78
jod35 <jodestrevin@gmail.com>
2021-07-29 05:27:27 +0300

    login with jwt

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,11 @@
-from flask import Flask,request
+from flask import Flask,request,jsonify
 from flask_restx import Api,Resource,fields
 from config import DevConfig
-from models import Recipe
+from models import Recipe,User
 from exts import db
 from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_jwt_extended import JWTManager,create_access_token,create_refresh_token,jwt_required
 
 app=Flask(__name__)
 app.config.from_object(DevConfig)
@@ -11,7 +13,7 @@ app.config.from_object(DevConfig)
 db.init_app(app)
 
 migrate=Migrate(app,db)
-
+JWTManager(app)
 
 api=Api(app,doc='/docs')
 
@@ -26,10 +28,77 @@ recipe_model=api.model(
 )
 
 
+signup_model=api.model(
+    'SignUp',
+    {
+        "username":fields.String(),
+        "email":fields.String(),
+        "password":fields.String()
+    }
+)
+
+
+login_model=api.model(
+    'Login',
+    {
+        'username':fields.String(),
+        'password':fields.String()
+    }
+)
+
 @api.route('/hello')
 class HelloResource(Resource):
     def get(self):
         return {"message":"Hello World"}
+
+
+@api.route('/signup')
+class SignUp(Resource):
+    @api.expect(signup_model)
+    def post(self):
+        data=request.get_json()
+
+
+        username=data.get('username')
+
+        db_user=User.query.filter_by(username=username).first()
+
+        if db_user is not None:
+            return jsonify({"message":f"User with username {username} already exists"})
+
+        new_user=User(
+            username=data.get('username'),
+            email=data.get('email'),
+            password=generate_password_hash(data.get('password'))
+        )
+
+        new_user.save()
+
+        return jsonify({"message":"User created successfuly"})
+        
+        
+
+@api.route('/login')
+class Login(Resource):
+
+    @api.expect(login_model)
+    def post(self):
+        data=request.get_json()
+
+        username=data.get('username')
+        password=data.get('password')
+
+        db_user=User.query.filter_by(username=username).first()
+
+        if db_user and check_password_hash(db_user.password, password):
+
+            access_token=create_access_token(identity=db_user.username)
+            refresh_token=create_refresh_token(identity=db_user.username)
+
+            return jsonify(
+                {"access_token":access_token,"refresh_token":refresh_token}
+            )
+
 
 
 
@@ -48,6 +117,8 @@ class RecipesResource(Resource):
 
 
     @api.marshal_with(recipe_model)
+    @api.expect(recipe_model)
+    @jwt_required()
     def post(self):
         """Create a new recipe"""
 
@@ -77,6 +148,7 @@ class RecipeResource(Resource):
 
 
     @api.marshal_with(recipe_model)
+    @jwt_required()
     def put(self,id):
         """Update a recipe by id """
         
@@ -91,6 +163,7 @@ class RecipeResource(Resource):
 
 
     @api.marshal_with(recipe_model)
+    @jwt_required()
     def delete(self,id):
         """Delete a recipe by id """
 
--- backend/models.py
+++ backend/models.py
@@ -51,3 +51,8 @@ class User(db.Model):
 
     def __repr__(self):
         return f"<User {self.username}>"
+
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
