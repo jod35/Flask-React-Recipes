54e68eb3de403a517b03464c4e63bdce9225f5ee
jod35 <jodestrevin@gmail.com>
2021-08-13 07:19:22 +0300

    form validations

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,85 +1,89 @@
 import React, { useState } from 'react'
 import { Form, Button, Alert } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
 
 
 const SignUpPage = () => {
-    const [username, setUsername] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [confirmPassword, setConfirmPassword] = useState('')
-    const [show, setShow] = useState(false)
-    const [serverResponse,setServerResponse]=useState('')
 
 
-    const submitForm = () => {
-        console.log("Form submitted");
-        console.log(username)
-        console.log(email)
-        console.log(password)
-        console.log(confirmPassword)
+
+    const { register, watch, handleSubmit, reset,formState: { errors } } = useForm();
+
+    const submitForm = (data) => {
+
+        console.log(data)
+
+        reset()
 
 
-        setUsername('')
-        setEmail('')
-        setPassword('')
-        setConfirmPassword('')
     }
+
+    console.log(watch("username"));
+    console.log(watch("email"));
+    console.log(watch("password"))
+    console.log(watch("confirmPassword"))
 
     return (
         <div className="container">
             <div className="form">
-                
-                {show? 
-                <Alert variant="danger" onClose={() => setShow(false)} dismissible>
-                    <p>
-                       {serverResponse}
-                    </p>
-                </Alert>:<><h1>Sign Up Page</h1></>
-                }
+
+                <h1>Sign Up Page</h1>
+
                 <form>
                     <Form.Group>
                         <Form.Label>Username</Form.Label>
                         <Form.Control type="text"
                             placeholder="Your username"
-                            value={username}
-                            name="username"
-                            onChange={(e) => { setUsername(e.target.value) }}
+                            {...register("username", { required: true, maxLength: 25 })}
                         />
+                        <br></br>
+                        {errors.username && <span style={{ color: "red" }}>Username is required</span>}
+                        <br></br>
+                        {errors.username?.type=="maxLength"&&<span style={{ color: "red" }}>Max characters should be 25</span>}
                     </Form.Group>
+
                     <br></br>
                     <Form.Group>
                         <Form.Label>Email</Form.Label>
                         <Form.Control type="email"
                             placeholder="Your email"
-                            value={email}
-                            name="email"
-                            onChange={(e) => { setEmail(e.target.value) }}
+                            {...register("email", { required: true, maxLength: 80 })}
                         />
+                        <br></br>
+                        {errors.email && <span style={{ color: "red" }}>Email is required</span>}
+                        <br></br>
+                        {errors.email?.type=="maxLength"&&<span style={{ color: "red" }}>Max characters should be 80</span>}
                     </Form.Group>
                     <br></br>
                     <Form.Group>
                         <Form.Label>Password</Form.Label>
                         <Form.Control type="password"
                             placeholder="Your password"
-                            value={password}
-                            name="password"
-                            onChange={(e) => { setPassword(e.target.value) }}
+                            {...register("password", { required: true, minLength: 8 })}
+
                         />
+                        <br></br>
+                        {errors.password && <span style={{ color: "red" }}>Password is required</span>}
+                        <br></br>
+                        {errors.password?.type=="minLength"&&<span style={{ color: "red" }}>Min characters should be 8</span>}
                     </Form.Group>
                     <br></br>
                     <Form.Group>
                         <Form.Label>Confirm Password</Form.Label>
                         <Form.Control type="password" placeholder="Your password"
-                            value={confirmPassword}
-                            name="confirmPassword"
-                            onChange={(e) => { setConfirmPassword(e.target.value) }}
+                            {...register("confirmPassword", { required: true, minLength: 8})}
+
 
                         />
+                        <br></br>
+                        {errors.confirmPassword && <span style={{ color: "red" }}>Confirm Password is required</span>}
+                        <br></br>
+                        {errors.confirmPassword?.type=="minLength"&&<span style={{ color: "red" }}>Min characters should be 8</span>}
                     </Form.Group>
                     <br></br>
                     <Form.Group>
-                        <Button as="sub" variant="primary" onClick={submitForm}>SignUp</Button>
+                        <Button as="sub" variant="primary" onClick={handleSubmit(submitForm)}>SignUp</Button>
                     </Form.Group>
                     <br></br>
                     <Form.Group>
