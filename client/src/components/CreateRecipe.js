2e79706198a27f1a1677e26a437edc506bc8c3b7
jod35 <jodestrevin@gmail.com>
2021-08-25 09:11:27 +0300

    deleting recipes

--- client/src/components/CreateRecipe.js
+++ client/src/components/CreateRecipe.js
@@ -1,60 +1,64 @@
-import React from 'react'
-import { Form,Button } from 'react-bootstrap'
-import {useForm} from 'react-hook-form'
+import React, { useState } from 'react'
+import { Form, Button } from 'react-bootstrap'
+import { useForm } from 'react-hook-form'
 
 
-const CreateRecipePage=()=>{
+const CreateRecipePage = () => {
 
-    const {register,handleSubmit,reset,formState:{errors}}=useForm()
+    const { register, handleSubmit, reset, formState: { errors } } = useForm()
+    const [show,setShow]=useState(false);
 
-    const createRecipe=(data)=>{
+    const createRecipe = (data) => {
         console.log(data)
 
-        const token=localStorage.getItem('REACT_TOKEN_AUTH_KEY');
+        const token = localStorage.getItem('REACT_TOKEN_AUTH_KEY');
         console.log(token)
 
 
-        const requestOptions={
-            method:'POST',
-            headers:{
-                'content-type':'application/json',
-                'Authorization':`Bearer ${JSON.parse(token)}`
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': `Bearer ${JSON.parse(token)}`
             },
-            body:JSON.stringify(data)
+            body: JSON.stringify(data)
 
         }
 
-        fetch('/recipe/recipes',requestOptions)
-        .then(res=>res.json())
-        .then(data=>console.log(data))
-        .catch(err=>console.log(err))
+        fetch('/recipe/recipes', requestOptions)
+            .then(res => res.json())
+            .then(data => {
+                reset()
+            })
+            .catch(err => console.log(err))
 
     }
 
-    return(
+    return (
         <div className="container">
+           
             <h1>Create A Recipe</h1>
             <form>
                 <Form.Group>
                     <Form.Label>Title</Form.Label>
-                    <Form.Control type="text" 
-                        {...register('title',{required:true,maxLength:25})}
+                    <Form.Control type="text"
+                        {...register('title', { required: true, maxLength: 25 })}
                     />
                 </Form.Group>
-                {errors.title && <p style={{color:'red'}}><small>Title is required</small></p>}
-                {errors.title?.type === "maxLength" && <p style={{color:'red'}}>
+                {errors.title && <p style={{ color: 'red' }}><small>Title is required</small></p>}
+                {errors.title?.type === "maxLength" && <p style={{ color: 'red' }}>
                     <small>Title should be less than 25 characters</small>
-                    </p>}
+                </p>}
                 <Form.Group>
                     <Form.Label>Description</Form.Label>
                     <Form.Control as="textarea" rows={5}
-                        {...register('description',{required:true,maxLength:255})}
+                        {...register('description', { required: true, maxLength: 255 })}
                     />
                 </Form.Group>
-                {errors.description && <p style={{color:'red'}}><small>Description is required</small></p>}
-                {errors.description?.type === "maxLength" && <p style={{color:'red'}}>
+                {errors.description && <p style={{ color: 'red' }}><small>Description is required</small></p>}
+                {errors.description?.type === "maxLength" && <p style={{ color: 'red' }}>
                     <small>Description should be less than 255 characters</small>
-                    </p>}
+                </p>}
                 <br></br>
                 <Form.Group>
                     <Button variant="primary" onClick={handleSubmit(createRecipe)}>
--- client/src/components/Recipe.js
+++ client/src/components/Recipe.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import { Card } from 'react-bootstrap';
+import { Button, Card ,Modal} from 'react-bootstrap';
 
 
-const Recipe=({title,description})=>{
+const Recipe=({title,description,onClick,onDelete})=>{
     return(
         <Card className="recipe">
             <Card.Body>
                 <Card.Title>{title}</Card.Title>
                 <p>{description}</p>
+                <Button variant='primary' onClick={onClick}>Update</Button>
+                {' '}
+                <Button variant='danger' onClick={onDelete}>Delete</Button>
             </Card.Body>
         </Card>
     )
