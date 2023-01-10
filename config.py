5c2ef355990db742c3166f0ac90c14bedceb7a0a
jod35 <jodestrevin@gmail.com>
2023-01-10 18:11:17 +0300

    change some settings

--- config.py
+++ config.py
@@ -2,24 +2,29 @@ from decouple import config
 import os
 from datetime import timedelta
 
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 class Config:
-    SECRET_KEY=config('SECRET_KEY')
-    SQLALCHEMY_TRACK_MODIFICATIONS=config('SQLALCHEMY_TRACK_MODIFICATIONS',cast=bool)
+    SECRET_KEY=os.getenv('SECRET_KEY',"secret")
+    SQLALCHEMY_TRACK_MODIFICATIONS=os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS',False)
 
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI="sqlite:///dev.db"
+    SQLALCHEMY_DATABASE_URI=os.getenv('DATABASE_URI',"sqlite:///dev.db")
     DEBUG=True
     # SQLALCHEMY_ECHO=True
 
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI="sqlite:///dev.db"
-    DEBUG=config('DEBUG',cast=bool)
-    SQLALCHEMY_ECHO=config('ECHO',cast=bool)
-    SQLALCHEMY_TRACK_MODIFICATIONS=config('SQLALCHEMY_TRACK_MODIFICATIONS',cast=bool)
+    SQLALCHEMY_DATABASE_URI=os.getenv('DATABASE_URI',"sqlite://prod.db")
+    DEBUG=os.getenv('DEBUG',False)
+    SQLALCHEMY_ECHO=os.getenv('ECHO',False)
+    SQLALCHEMY_TRACK_MODIFICATIONS=os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS',False)
 
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI='sqlite:///test.db'
