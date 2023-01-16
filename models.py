b7c829c40c4b95a044f61662bccdbba4d496c38a
jod35 <jodestrevin@gmail.com>
2023-01-16 13:25:25 +0300

    add docstrings

--- models.py
+++ models.py
@@ -19,14 +19,39 @@ class Recipe(db.Model):
 
 
     def save(self):
+        """
+        The save function is used to save the changes made to a model instance.
+        It takes in no arguments and returns nothing.
+        
+        :param self: Refer to the current instance of the class
+        :return: The object that was just saved
+        :doc-author:jod35
+        """
         db.session.add(self)
         db.session.commit()
 
     def delete(self):
+        """
+        The delete function is used to delete a specific row in the database. It takes no parameters and returns nothing.
+        
+        :param self: Refer to the current instance of the class, and is used to access variables that belongs to the class
+        :return: Nothing
+        :doc-author:jod35
+        """
         db.session.delete(self)
         db.session.commit()
 
     def update(self,title,description):
+        """
+        The update function updates the title and description of a given blog post.
+        It takes two parameters, title and description.
+        
+        :param self: Access variables that belongs to the class
+        :param title: Update the title of the post
+        :param description: Update the description of the blog post
+        :return: A dictionary with the updated values of title and description
+        :doc-author:jod35
+        """
         self.title=title
         self.description=description
 
@@ -50,6 +75,10 @@ class User(db.Model):
     password=db.Column(db.Text(),nullable=False)
 
     def __repr__(self):
+        """
+            returns string rep of object
+
+        """
         return f"<User {self.username}>"
 
 
